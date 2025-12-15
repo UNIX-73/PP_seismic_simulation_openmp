3b2c8324a6c8c556a835e95b837a25f17005b308
Unai <iborra.unai@gmail.com>
2025-12-15 16:19:35 +0100

    atomic test

--- src/parallel.c
+++ src/parallel.c
@@ -1226,28 +1226,28 @@ void smvp(int nodes, double ***A, int *Acol, int *Aindex, double **v,
 				sum2 += A[Anext][2][0] * v[col][0] +
 						A[Anext][2][1] * v[col][1] + A[Anext][2][2] * v[col][2];
 
-#pragma omp critical
-				{
-					w[col][0] += A[Anext][0][0] * v[i][0] +
-								 A[Anext][1][0] * v[i][1] +
-								 A[Anext][2][0] * v[i][2];
-					w[col][1] += A[Anext][0][1] * v[i][0] +
-								 A[Anext][1][1] * v[i][1] +
-								 A[Anext][2][1] * v[i][2];
-					w[col][2] += A[Anext][0][2] * v[i][0] +
-								 A[Anext][1][2] * v[i][1] +
-								 A[Anext][2][2] * v[i][2];
-				}
+#pragma omp atomic
+				w[col][0] += A[Anext][0][0] * v[i][0] +
+							 A[Anext][1][0] * v[i][1] +
+							 A[Anext][2][0] * v[i][2];
+#pragma omp atomic
+				w[col][1] += A[Anext][0][1] * v[i][0] +
+							 A[Anext][1][1] * v[i][1] +
+							 A[Anext][2][1] * v[i][2];
+#pragma omp atomic
+				w[col][2] += A[Anext][0][2] * v[i][0] +
+							 A[Anext][1][2] * v[i][1] +
+							 A[Anext][2][2] * v[i][2];
 
 				Anext++;
 			}
 
-#pragma omp critical
-			{
-				w[i][0] += sum0;
-				w[i][1] += sum1;
-				w[i][2] += sum2;
-			}
+#pragma omp atomic
+			w[i][0] += sum0;
+#pragma omp atomic
+			w[i][1] += sum1;
+#pragma omp atomic
+			w[i][2] += sum2;
 		}
 	}
 }
