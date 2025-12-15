e8adcfb2bd375b62088aaabae475dea6c82ec631
Unai <iborra.unai@gmail.com>
2025-12-15 17:00:02 +0100

    t3

--- src/parallel.c
+++ src/parallel.c
@@ -433,13 +433,13 @@ int main(int argc, char **argv)
 
 #pragma omp parallel private(i, j)
 		{
-#pragma omp for private(i, j)
+#pragma omp for
 			for (i = 0; i < ARCHnodes; i++)
 				for (j = 0; j < 3; j++) {
 					disp[disptplus][i][j] *= -Exc.dt * Exc.dt;
 				}
 
-#pragma omp for private(i, j)
+#pragma omp for
 			for (i = 0; i < ARCHnodes; i++)
 				for (j = 0; j < 3; j++)
 					disp[disptplus][i][j] +=
@@ -451,13 +451,13 @@ int main(int argc, char **argv)
 							 C23[i][j] * phi1(time) / 2.0 +
 							 V23[i][j] * phi0(time) / 2.0);
 
-#pragma omp for private(i, j)
+#pragma omp for
 			for (i = 0; i < ARCHnodes; i++)
 				for (j = 0; j < 3; j++)
 					disp[disptplus][i][j] = disp[disptplus][i][j] /
 											(M[i][j] + Exc.dt / 2.0 * C[i][j]);
 
-#pragma omp for private(i, j)
+#pragma omp for
 			for (i = 0; i < ARCHnodes; i++)
 				for (j = 0; j < 3; j++)
 					vel[i][j] =
